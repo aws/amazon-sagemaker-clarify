1c2344be1c6156575ac9ac2a74b935c459a06361
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2020-11-23 22:35:12 -0800

    Add commit_hash file to the package_wheel (#93)
    * add commit_hash file to the package_wheel
    
    Co-authored-by: Satish Gollaprolu <sgollapr@amazon.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
 # Licensed under the Amazon Software License  http://aws.amazon.com/asl/
-
+import os
 from setuptools import find_packages, setup
 
 INSTALL_REQUIRES = ["boto3", "pyarrow", "pandas", "s3fs", "numpy", "sklearn", "pyfunctional"]
@@ -29,12 +29,14 @@ EXTRAS_REQUIRE = {
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
 
+data_files = ["COMMIT_HASH"] if os.path.exists("./COMMIT_HASH") else []
 
 setup(
     name="smclarify",
     version="0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    data_files=[("", data_files)],
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     install_requires=INSTALL_REQUIRES,
